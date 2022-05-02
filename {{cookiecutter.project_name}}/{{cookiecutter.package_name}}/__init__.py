ffbc423711ca51fc128a955de6fe21dad72409bf
gtca <bredikhin.daniel@gmail.com>
2022-05-02 11:50:56 +0200

    Add simple API scaffold with pp, tl, and pl

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,1 +1,5 @@
 __version__ = "0.0.1"
+
+from . import preprocessing as pp
+from . import tools as tl
+from . import plotting as pl
---
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/plotting/basic.py
@@ -0,0 +1,8 @@
+from anndata import AnnData
+
+def basic_plot(adata: AnnData):
+    """
+    Generate a basic plot for an AnnData object.
+    """
+    print("Import matplotlib and implementa a plotting function here.")
+    return 0
