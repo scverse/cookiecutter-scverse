7dedbdd6fd2aaf499573192969058ca40368baea
Philipp A <flying-sheep@web.de>
2022-05-03 10:55:48 +0200

    Update __init__.py

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,5 +1,9 @@
+from importlib.metadata import version
+
 from . import pp
 from . import tl
 from . import pl
 
-__version__ = "0.0.1"
+__all__ = ["pp", "tl", "pl"]
+
+__version__ = version("{{ cookiecutter.project_name }}")
