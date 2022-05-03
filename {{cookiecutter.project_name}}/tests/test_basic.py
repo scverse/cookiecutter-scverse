8d7f9679cc2b0679884dd3ab8a7d53c065db19ac
Michal Klein <michal.klein@protonmail.com>
2022-05-03 09:52:14 +0200

    Fix flake8

--- {{cookiecutter.project_name}}/tests/test_basic.py
+++ {{cookiecutter.project_name}}/tests/test_basic.py
@@ -1,9 +1,9 @@
 import {{cookiecutter.package_name}}
 
 
-def test_package_has_version():  # noqa: D103
+def test_package_has_version():
     {{cookiecutter.package_name}}.__version__
 
 
-def test_fails():  # noqa: D103
+def test_fails():
     assert 1 == 0
