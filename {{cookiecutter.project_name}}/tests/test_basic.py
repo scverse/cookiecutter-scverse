6c100d670eff358f22941f1a01f3cc0fdecf4b09
giovp <giov.pll@gmail.com>
2022-05-17 14:08:49 +0200

    add decorator to make test pass

--- {{cookiecutter.project_name}}/tests/test_basic.py
+++ {{cookiecutter.project_name}}/tests/test_basic.py
@@ -1,9 +1,9 @@
 import {{cookiecutter.package_name}}
-
+import pytest
 
 def test_package_has_version():
     {{cookiecutter.package_name}}.__version__
 
-
-def test_fails():
-    assert 1 == 0
+@pytest.mark.skip(reason="This decorator should be removed when test passes.")
+def test_example():
+    assert 1 == 0 # This test is designed to fail.
