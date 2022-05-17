e80dd8cb1e141f655023dc802a740f28e825d538
Giovanni Palla <giov.pll@gmail.com>
2022-05-17 15:29:17 +0200

    add main as default for docs and minor fixes (#58)

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git init .
+git init --initial-branch=main .
--- {{cookiecutter.project_name}}/tests/test_basic.py
+++ {{cookiecutter.project_name}}/tests/test_basic.py
@@ -1,3 +1,5 @@
+import pytest
+
 import {{cookiecutter.package_name}}
 
 
@@ -5,5 +7,6 @@ def test_package_has_version():
     {{cookiecutter.package_name}}.__version__
 
 
-def test_fails():
-    assert 1 == 0
+@pytest.mark.skip(reason="This decorator should be removed when test passes.")
+def test_example():
+    assert 1 == 0  # This test is designed to fail.
