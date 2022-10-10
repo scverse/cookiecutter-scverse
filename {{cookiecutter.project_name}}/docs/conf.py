f502b632c9eb453eca0fa2aa75c04d1130e3227e
adamgayoso <adamgayoso@users.noreply.github.com>
2022-10-10 10:18:57 -0700

    remove linkcode

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -5,12 +5,6 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
-from typing import Any
-import subprocess
-import os
-import importlib
-import inspect
-import re
 import sys
 from datetime import datetime
 from importlib.metadata import metadata
@@ -56,7 +50,6 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
-    "sphinx.ext.linkcode",
     "sphinxcontrib.bibtex",
     "sphinx_autodoc_typehints",
     "sphinx.ext.mathjax",
@@ -104,58 +97,6 @@ intersphinx_mapping = {
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 
 
-# -- Linkcode settings -------------------------------------------------
-
-
-def git(*args):
-    """Run a git command and return the output."""
-    return subprocess.check_output(["git", *args]).strip().decode()
-
-
-# https://github.com/DisnakeDev/disnake/blob/7853da70b13fcd2978c39c0b7efa59b34d298186/docs/conf.py#L192
-# Current git reference. Uses branch/tag name if found, otherwise uses commit hash
-git_ref = None
-try:
-    git_ref = git("name-rev", "--name-only", "--no-undefined", "HEAD")
-    git_ref = re.sub(r"^(remotes/[^/]+|tags)/", "", git_ref)
-except Exception:  # noqa: B902
-    pass
-
-# (if no name found or relative ref, use commit hash instead)
-if not git_ref or re.search(r"[\^~]", git_ref):
-    try:
-        git_ref = git("rev-parse", "HEAD")
-    except Exception:  # noqa: B902
-        git_ref = "main"
-
-# https://github.com/DisnakeDev/disnake/blob/7853da70b13fcd2978c39c0b7efa59b34d298186/docs/conf.py#L192
-github_repo = "https://github.com/" + html_context["github_user"] + "/" + project_name
-_project_module_path = os.path.dirname(importlib.util.find_spec(package_name).origin)  # type: ignore
-
-
-def linkcode_resolve(domain, info):
-    """Resolve links for the linkcode extension."""
-    if domain != "py":
-        return None
-
-    try:
-        obj: Any = sys.modules[info["module"]]
-        for part in info["fullname"].split("."):
-            obj = getattr(obj, part)
-        obj = inspect.unwrap(obj)
-
-        if isinstance(obj, property):
-            obj = inspect.unwrap(obj.fget)  # type: ignore
-
-        path = os.path.relpath(inspect.getsourcefile(obj), start=_project_module_path)  # type: ignore
-        src, lineno = inspect.getsourcelines(obj)
-    except Exception:  # noqa: B902
-        return None
-
-    path = f"{path}#L{lineno}-L{lineno + len(src) - 1}"
-    return f"{github_repo}/blob/{git_ref}/src/{package_name}/{path}"
-
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -165,6 +106,7 @@ html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
 html_title = project_name
 
+github_repo = "https://github.com/" + html_context["github_user"] + "/" + project_name
 html_theme_options = {
     "repository_url": github_repo,
     "use_repository_button": True,
