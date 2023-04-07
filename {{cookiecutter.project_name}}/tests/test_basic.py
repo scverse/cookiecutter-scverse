126c87fb63c1d037d294d9f3ea8fdc6cd6120c11
Gregor Sturm <mail@gregor-sturm.de>
2023-04-07 08:26:54 +0200

    Fix ruff complaining about "useless statement" in test (#159)

--- {{cookiecutter.project_name}}/tests/test_basic.py
+++ {{cookiecutter.project_name}}/tests/test_basic.py
@@ -4,7 +4,7 @@ import {{cookiecutter.package_name}}
 
 
 def test_package_has_version():
-    {{cookiecutter.package_name}}.__version__
+    assert {{cookiecutter.package_name}}.__version__ is not None
 
 
 @pytest.mark.skip(reason="This decorator should be removed when test passes.")
