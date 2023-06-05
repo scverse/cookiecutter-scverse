b4b78b5bbe2dba81645ca5564c3bec7338ebdd1f
Philipp A <flying-sheep@web.de>
2023-06-05 14:44:53 +0200

    add social card image path

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -108,7 +108,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_book_theme"
-html_static_path = ["_static"]
+html_static_path = ["_static", "_images"]
 html_title = project_name
 
 html_theme_options = {
