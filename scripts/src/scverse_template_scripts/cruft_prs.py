4fcf46675ea727221ec35a30e2d5c22859a1ca5d
Gregor Sturm <mail@gregor-sturm.de>
2023-08-09 20:51:33 +0200

    Fix global exception handler

--- scripts/src/scverse_template_scripts/cruft_prs.py
+++ scripts/src/scverse_template_scripts/cruft_prs.py
@@ -265,7 +265,7 @@ def main(tag_name: str) -> None:
             make_pr(con, release, repo_url)
         except Exception as e:
             failed += 1
-            log.error(f"Error updating {repo_url}.", e)
+            log.exception(f"Error updating {repo_url}.", e)
 
     sys.exit(failed > 0)
 
