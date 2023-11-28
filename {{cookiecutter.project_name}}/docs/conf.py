765c25c838914413dd1aee3d7ae37357d890d0de
Gregor Sturm <mail@gregor-sturm.de>
2023-11-28 17:09:51 +0100

    Reduce font size in data frames (#250)
    Co-authored-by: Philipp A <flying-sheep@web.de>

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -109,6 +109,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 #
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
+
 html_title = project_name
 
 html_theme_options = {
